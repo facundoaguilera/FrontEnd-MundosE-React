69e56dffa77bd7fabb2aa2da978ab11ce2fb7239
facundoaguilera <75026725+facundoaguilera@users.noreply.github.com>
2022-02-11 09:39:57 -0300

    subido por las dudas por un bug que vi

--- pin-react/src/components/seccion5/Formulario.js
+++ pin-react/src/components/seccion5/Formulario.js
@@ -4,18 +4,20 @@ import Button from "../Button";
 
 function Formulario(params) {
     const [data, setData] = useState({"name":"","email":"","phone":"","message":"" })
+
     const handleInputChange = (event) => {
-        
         setData({
             ...data, //esto realiza una copia de data
             [event.target.name]:event.target.value
         })
     }
+    
+
     const sendData = (event) => { 
         event.preventDefault();
         console.log(data.name+" "+data.email+" "+data.phone+ " "+data.message)
         const Url="http://pin.cardansel.com/laravel/public/api/comments";
-        const Url2="http://127.0.0.1:8000/api/comment"
+        const Url2="http://127.0.0.1:8000/api/comment";
         axios.post(Url2, data).then(response => {console.log(response);
         console.log(response.data);} )
     };
@@ -30,10 +32,7 @@ function Formulario(params) {
             <Button className="ReadMore" type="submit">
             Send
             </Button>
-            {/* <button className="ReadMore" type="submit"><p>
-                          Send
-                        </p>
-            </button> */}
+            
             </div>
         </form>
         
